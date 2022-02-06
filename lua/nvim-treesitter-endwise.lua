59c8a6eaeae282073a7435a542334a119bfb1ea5
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2022-02-06 16:15:50 -0500

    Fix #10

--- lua/nvim-treesitter-endwise.lua
+++ lua/nvim-treesitter-endwise.lua
@@ -1,3 +1,5 @@
+local queries = require("nvim-treesitter.query")
+
 local M = {}
 
 function M.init()
@@ -6,6 +8,34 @@ function M.init()
             module_path = 'nvim-treesitter.endwise',
             enable = false,
             disable = {},
+            is_supported = function(lang)
+                if queries.has_query_files(lang, 'endwise') then
+                    return true
+                end
+
+                local seen = {}
+                local function has_nested_endwise_language(nested_lang)
+                    if seen[nested_lang] then
+                        return false
+                    end
+                    seen[nested_lang] = true
+
+                    if queries.has_query_files(lang, 'injections') then
+                        local query = queries.get_query(lang, 'injections')
+                        for _, capture in ipairs(query.info.captures) do
+                            if capture == 'language'
+                                or queries.has_query_files(lang, 'endwise')
+                                or has_nested_endwise_language(lang) then
+                                return true
+                            end
+                        end
+                    end
+
+                    return false
+                end
+
+                return has_nested_endwise_language(lang)
+            end,
         }
     }
 end
