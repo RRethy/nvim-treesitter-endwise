53d6b3d23af633f46d20bc87883561598c5efddf
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2023-06-23 15:45:56 +0100

    feat: duplicate lua config for fish

---
+++ queries/fish/endwise.scm
@@ -0,0 +1,12 @@
+((function_declaration parameters: (_) @cursor) @endable @indent (#endwise! "end"))
+((function_definition parameters: (_) @cursor) @endable @indent (#endwise! "end"))
+((while_statement "do" @cursor) @endable @indent (#endwise! "end"))
+((for_statement "do" @cursor) @endable @indent (#endwise! "end"))
+((if_statement "then" @cursor) @endable @indent (#endwise! "end"))
+((do_statement "do" @cursor) @endable @indent (#endwise! "end"))
+
+((ERROR ("function" . (_)? . (parameters) @cursor) @indent) (#endwise! "end"))
+((ERROR ("do" @cursor @indent)) (#endwise! "end"))
+((ERROR ("while" @indent . (_) . "do" @cursor)) (#endwise! "end"))
+((ERROR ("for" @indent . [(for_generic_clause) (for_numeric_clause)] . "do" @cursor)) (#endwise! "end"))
+((ERROR ("if" @indent . (_) . "then" @cursor)) (#endwise! "end"))
